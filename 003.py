08e0b29e0ef0f2733f69c708a074f4d9e79822bd
Jim Lawton <jim.lawton@gmail.com>
2014-06-30 14:05:59 +0100

    Add 003 solution.

---
+++ 003.py
@@ -0,0 +1,24 @@
+"""
+Project Euler Problem #3
+=========================
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+"""
+
+def primefactors(n):
+    """Return the prime factors of a positive integer."""
+    factors = []
+    d = 2
+    while n > 1:
+        while n % d == 0:
+            factors.append(d)
+            n /= d
+        d = d + 1
+    return factors
+
+
+pfactors = primefactors(600851475143)
+print max(pfactors) 
+
