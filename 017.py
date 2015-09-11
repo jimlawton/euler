d997dc91bf3006bf7049f48d7fa2d4e2e9601c48
Jim Lawton <jim.lawton@gmail.com>
2015-09-11 18:04:47 +0100

    Add problem 17 solution.

--- 017.py
+++ 017.py
@@ -14,4 +14,46 @@ forty-two) contains 23 letters and 115 (one hundred and fifteen) contains
 with British usage.
 """
 
+import sys
 
+
+END = 1000
+
+words = {
+    1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven",
+    8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve",
+    13: "thirteen", 14: "fourteen", 15: "fifteen", 16: "sixteen",
+    17: "seventeen", 18: "eighteen", 19: "nineteen",
+    20: "twenty", 30: "thirty", 40: "forty", 50: "fifty", 60: "sixty",
+    70: "seventy", 80: "eighty", 90: "ninety", 100: "one hundred",
+    1000: "one thousand"
+}
+
+def num2words(n):
+    numstr = str(n)
+    if n in words:
+        return words[n]
+    else:
+        if len(numstr) == 2:
+            tens = int(numstr[0])
+            units = int(numstr[1])
+            return words[tens * 10] + '-' + words[units]
+        elif len(numstr) == 3:
+            hundreds = int(numstr[0])
+            remainder = n - hundreds * 100
+            if remainder == 0:
+                return words[hundreds] + " hundred"
+            else:
+                return words[hundreds] + " hundred and " + num2words(remainder)
+        else:
+            sys.exit(">1000 not supported yet!")
+
+
+numlist = []
+for i in range(1, END + 1):
+    numlist.append(num2words(i))
+
+filtered = ' '.join(numlist)
+filtered = filtered.replace('-', '')
+filtered = filtered.replace(' ', '')
+print len(filtered)
